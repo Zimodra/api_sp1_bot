f56533dc2d929dcc793b1d2cb7ef56c0ad0e46a5
Зимодра Ксения <ksuta.mail@mail.ru>
2021-05-23 16:06:47 +1000

    Спринт 8

--- homework.py
+++ homework.py
@@ -14,12 +14,12 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
-
 logging.basicConfig(
     level=logging.DEBUG,
-    filename='program.log', 
+    filename='program.log',
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 )
+
 
 def parse_homework_status(homework):
     name = homework.get('homework_name')
@@ -31,7 +31,8 @@ def parse_homework_status(homework):
     elif status == 'reviewing':
         verdict = 'Работа взята в ревью.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = 'Ревьюеру всё понравилось,'
+        'можно приступать к следующему уроку.'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
@@ -44,22 +45,27 @@ def get_homework_statuses(current_timestamp):
         return homework_statuses.json()
     return {}
 
+
 def send_message(message, bot_client):
     return bot_client.send_message(CHAT_ID, message)
     logging.info('Сообщение отправлено')
 
 
 def main():
-    bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
+    telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+                send_message(parse_homework_status(
+                    new_homework.get('homeworks')[0]
+                ))
+            current_timestamp = new_homework.get(
+                'current_date', current_timestamp
+            )
+            time.sleep(300)
 
         except Exception as e:
             message = f'Бот столкнулся с ошибкой: {e}'
